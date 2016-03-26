a095bd10e90745bdadad79383f12d242f06502fd
Alex Luecke <me@alexjl.com>
2016-03-25 18:09:23 -0700

    Bind DOM elements to model data. `price` -> `cost`. Added `addSellable` function to enable user to create additional sellable items.

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -2,34 +2,34 @@ var MovieTheaterApp = angular.module('MovieTheaterApp', []);
 
 MovieTheaterApp.controller('MovieTheaterCtrl', function ($scope) {
 
+	// Model data
+	$scope.inputs = { 'name': '', 'cost': '' };
 	$scope.total = 0.0;
-
 	$scope.cart = {};
-
 	$scope.sellables = [
-		{ 'name': 'popcorn', 'price': 3.0 },
-		{ 'name': 'snickers', 'price': 4.0 },
-		{ 'name': 'soda', 'price': 2.0 }
+		{ 'name': 'popcorn', 'cost': 3.0 },
+		{ 'name': 'snickers', 'cost': 4.0 },
+		{ 'name': 'soda', 'cost': 2.0 }
 	];
 
+	// Initialize cart items:
+	$scope.sellables.forEach(function(x) {
+		$scope.cart[x.name] = 0;
+	});
+
 	var defaultCostFunc = function(x) {
-		return x.price * $scope.cart[x.name];
+		return x.cost * $scope.cart[x.name];
 	};
 
 	var costFunctions = {
 		'snickers': function(x) {
-			return x.price * (
+			return x.cost * (
 				3 * Math.floor($scope.cart[x.name]/5) + $scope.cart[x.name]%5
 			);
 		},
 		'popcorn': defaultCostFunc,
 		'soda': defaultCostFunc,
 	};
-
-	// Initialize cart items:
-	$scope.sellables.forEach(function(x) {
-		$scope.cart[x.name] = 0;
-	});
 
 	$scope.addToCart = function(name) {
 		if ($scope.cart[name] !== 'undefined')
@@ -47,7 +47,7 @@ MovieTheaterApp.controller('MovieTheaterCtrl', function ($scope) {
 
 	$scope.updateTotal = function() {
 		// Take each sellable item, do a lookup in the cart to see how many items
-		// have been added, multiply the price by the number added, and sum the
+		// have been added, multiply the cost by the number added, and sum the
 		// total.
 		$scope.total = $scope.sellables.map(function(x) {
 			// There are specials on particular
@@ -55,5 +55,17 @@ MovieTheaterApp.controller('MovieTheaterCtrl', function ($scope) {
 		}).reduce(function(x,y) {
 			return x+y;
 		});
+	};
+
+	$scope.addSellable = function() {
+		if ($scope.inputs.name.trim() && Number.parseFloat($scope.inputs.cost)) {
+			$scope.sellables.push({
+				'name': $scope.inputs.name,
+				'cost': Number.parseFloat($scope.inputs.cost),
+			});
+			costFunctions[$scope.inputs.name] = defaultCostFunc;
+			$scope.cart[$scope.inputs.name] = 0;
+			$scope.inputs = { 'name': '', 'cost': '' };
+		}
 	};
 });
